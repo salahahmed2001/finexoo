e55f401e8cf25b376a8ce6fe2b3dc8b61eae1e5e
salahahmed2001 <salah01068441@gmail.com>
2024-07-24 02:04:21 +0300

    6

--- src/Components/NotFound.js
+++ src/Components/NotFound.js
@@ -9,7 +9,7 @@ const NotFound = () => {
       <h2 className=''>File not found</h2>
       <p>The site configured at this address does not contain the requested file.</p>
       <p>If this is your site, make sure that the filename case matches the URL as well as any file permissions.</p>
-      <p>For root URLs (like http://example.com/) you must provide an index.html file.</p>
+      <p>For root URLs like http://example.com/ you must provide an index.html file.</p>
       <div className='not-found-btn'>
       <Link to='/'>Home</Link>
       </div>
